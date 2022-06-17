4945985464b9d3a88367acf75d8333885bbd59c9
Keith Light <keith.light@proagrica.com>
2022-06-17 16:40:05 +0100

    Amend output to use paths, update packages

--- build-tools/frontend/gulpfile.js
+++ build-tools/frontend/gulpfile.js
@@ -6,87 +6,85 @@ const browserify = require("browserify");
 const buffer = require("vinyl-buffer");
 const cssnano = require("cssnano");
 const gulp = require("gulp");
-// const imagemin = require("gulp-imagemin");
 const moduleImporter = require("sass-module-importer");
 const postcss = require("gulp-postcss");
 const rename = require("gulp-rename");
 const runSequence = require("gulp4-run-sequence").use(gulp);
-const sass = require('gulp-sass')(require('sass'));
+const sass = require("gulp-sass")(require("sass"));
 const source = require("vinyl-source-stream");
 const sourcemaps = require("gulp-sourcemaps");
 const terser = require("gulp-terser");
 
 const paths = {
-    input_scss: ["./scss/style.scss"],
-    input_js: ["./js/script.js"],
-    input_images: ["./images/**/*"],
+  input_scss: "./scss/style.scss",
+  output_css: "../wwwroot/css",
+  input_js: "./js/script.js",
+  output_js: "../wwwroot/js",
+  input_images: "./images/**/*",
+  output_images: "../wwwroot/images",
 };
 
 function styles(done) {
-    const plugins = [
-        autoprefixer({
-            overrideBrowserslist: ["last 3 version"],
-            remove: false,
-        }),
-        cssnano({
-            discardComments: {
-                removeAll: true,
-            },
-        }),
-    ];
+  const plugins = [
+    autoprefixer({
+      overrideBrowserslist: ["last 3 version"],
+      remove: false,
+    }),
+    cssnano({
+      discardComments: {
+        removeAll: true,
+      },
+    }),
+  ];
 
-    gulp.src(paths.input_scss)
-        .pipe(sourcemaps.init({ loadMaps: true }))
-        .pipe(sass({ importer: moduleImporter() }))
-        .pipe(sass().on("error", sass.logError))
-        .pipe(postcss(plugins))
-        .pipe(rename({ dirname: "./" }))
-        .pipe(sourcemaps.write("./"))
-        .pipe(gulp.dest("../wwwroot/css"));
+  gulp
+    .src(paths.input_scss)
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(sass({ importer: moduleImporter() }))
+    .pipe(sass().on("error", sass.logError))
+    .pipe(postcss(plugins))
+    .pipe(rename({ dirname: "./" }))
+    .pipe(sourcemaps.write("./"))
+    .pipe(gulp.dest(paths.output_css));
 
-    done();
+  done();
 }
 
 function scripts() {
-    // Added babelify to allow for ES6 imports and classes
-    return browserify({
-        entries: paths.input_js,
-        debug: true,
-    })
+  // Added babelify to allow for ES6 imports and classes
+  return browserify({
+    entries: paths.input_js,
+    debug: true,
+  })
     .transform(
-        babelify.configure({
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-            plugins: ["@babel/plugin-proposal-class-properties"],
-        })
+      babelify.configure({
+        presets: ["@babel/preset-env", "@babel/preset-react"],
+        plugins: ["@babel/plugin-proposal-class-properties"],
+      })
     )
-        .bundle()
-        .pipe(source("script.js"))
-        .pipe(buffer())
-        .pipe(sourcemaps.init({ loadMaps: true }))
-        .pipe(terser())
-        .pipe(sourcemaps.write("./"))
-        .pipe(gulp.dest("../wwwroot/js"));
+    .bundle()
+    .pipe(source("script.js"))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(terser())
+    .pipe(sourcemaps.write("./"))
+    .pipe(gulp.dest(paths.output_js));
 }
 
-// function images() {
-//     gulp.src(paths.input_images).pipe(imagemin()).pipe(gulp.dest("../wwwroot/images/"));
-// }
-
 function watch() {
-    runSequence(["styles", "scripts"]);
+  runSequence(["styles", "scripts"]);
 
-    gulp.watch("./scss/**/*.scss", styles);
-    gulp.watch("./js/**/*.js", scripts);
+  gulp.watch("./scss/**/*.scss", styles);
+  gulp.watch("./js/**/*.js", scripts);
 }
 
 function build(done) {
-    runSequence(["styles", "scripts"]);
+  runSequence(["styles", "scripts"]);
 
-    done();
+  done();
 }
 
 exports.build = build;
-// exports.images = images;
 exports.styles = styles;
 exports.scripts = scripts;
 exports.watch = watch;
